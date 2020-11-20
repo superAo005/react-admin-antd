9b16ff76c74fc50fab98f48999c0ffbe79a97a33
你上我下滴滴答 <xuyajunling@qq.com>
2020-11-20 10:02:51 +0800

    第一次提交

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, addDecoratorsLegacy } = require('customize-cra');
-module.exports = override(
- addDecoratorsLegacy()
-)
+const { override, addDecoratorsLegacy } = require('customize-cra')
+
+// 配置装饰器
+module.exports = override(addDecoratorsLegacy())
