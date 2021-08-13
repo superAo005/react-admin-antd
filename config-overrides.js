ff3d10ba1b79fbe58dc10a8ea2624a0735e8fe8b
zhenwangYang <zhenwangyang@trip.com>
2021-08-13 11:15:06 +0800

    pref: 添加Prettier ESLint 关注代码质量，Prettier 关注代码风格。

--- config-overrides.js
+++ config-overrides.js
@@ -4,11 +4,13 @@ const {
   addWebpackAlias,
   fixBabelImports,
   addLessLoader,
-} = require("customize-cra");
+  useEslintRc,
+  disableEsLint,
+} = require('customize-cra')
 
-const path = require("path");
+const path = require('path')
 function pathResolve(pathUrl) {
-  return path.join(__dirname, pathUrl);
+  return path.join(__dirname, pathUrl)
 }
 
 module.exports = override(
@@ -16,23 +18,26 @@ module.exports = override(
   addDecoratorsLegacy(),
   // 配置目录别名
   addWebpackAlias({
-    "@": pathResolve("./src"),
+    '@': pathResolve('./src'),
   }),
   // antd组件css样式全局引入
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: "css",
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
   }),
   addLessLoader({
     lessOptions: {
       javascriptEnabled: true,
       relativeUrls: false,
-      modifyVars: { "@primary-color": "#A80000" },
+      modifyVars: { '@primary-color': '#A80000' },
       // cssModules: {
       //   // if you use CSS Modules, and custom `localIdentName`, default is '[local]--[hash:base64:5]'.
       //   localIdentName: "[path][name]__[local]--[hash:base64:5]",
       // }
     },
-  })
-);
+  }),
+  // 注意，一定要用 path.resolve 引入eslint的配置文件，否则不生效
+  // useEslintRc(pathResolve("./.eslintrc.json"))
+  disableEsLint()
+)
