a0b5b79d1842d3666aea7fa4a17e91f73c90bb7f
zhenwangYang <zhenwangyang@trip.com>
2022-06-16 18:47:25 +0800

    feat:优化

--- src/redux/reducers/colorReducers.js
+++ src/redux/reducers/colorReducers.js
@@ -1,17 +1,17 @@
-import themeColor from "@/redux/state/themeColor";
+import themeColor from '@/redux/state/themeColor'
 
 const colorReducers = (state = { ...themeColor }, action) => {
   if (!state)
     return {
-      themeColor: "red",
-    };
+      themeColor: 'red',
+    }
 
   switch (action.type) {
-    case "CHANGE_COLOR":
-      return { ...state, themeColor: action.themeColor }; // 这里action传入动态色值并同步到state
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor } // 这里action传入动态色值并同步到state
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default colorReducers;
+export default colorReducers
